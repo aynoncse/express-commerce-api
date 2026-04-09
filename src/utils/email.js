0d23323c584d94ef6d0650815b01ed6d2bf5a6c2
aynoncse <aynoncse@gmail.com>
2026-04-10 01:49:16 +0600

    Add password reset functionality (forgot/reset)

--- src/utils/email.js
+++ src/utils/email.js
@@ -11,11 +11,20 @@ const transporter = nodemailer.createTransport({
 });
 
 const sendConfirmationEmail = async (order, userEmail) => {
-    await transporter.sendMail({
-        to: userEmail,
-        subject: 'Order Confirmation',
-        text: `Your order with ID ${order.id} has been confirmed!`,
-    });
+  await transporter.sendMail({
+    to: userEmail,
+    subject: 'Order Confirmation',
+    text: `Your order with ID ${order.id} has been confirmed!`,
+  });
 };
 
-module.exports = { sendConfirmationEmail };
+const sendPasswordResetEmail = async (userEmail, resetUrl) => {
+  await transporter.sendMail({
+    to: userEmail,
+    subject: 'Password Reset Request',
+    text: `You requested a password reset. Use the link below to reset your password:\n\n${resetUrl}\n\nIf you did not request this, please ignore this message.`,
+    html: `<p>You requested a password reset.</p><p>Click the link below to reset your password:</p><p><a href="${resetUrl}">${resetUrl}</a></p><p>If you did not request this, please ignore this message.</p>`,
+  });
+};
+
+module.exports = { sendConfirmationEmail, sendPasswordResetEmail };
